646affdc9356df95ab0925e7bb5b8fec54aa3129
Andrew Ramires May <andrewc3po@gmail.com>
2019-11-16 03:20:42 -0400

    Correção no servidor

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = process.env.npm_package_name;
+const nomeApp = 'transicao-penal';
 const app = express();
  
-app.use(express.static(`$/dist/$`));
- 
-app.get('/*', (req, res) => {
-res.sendFile(path.join(`$/dist/$/index.html`));
+// app.use(express.static(`$/dist/$`));
+app.use(express.static(__dirname + '/dist/'));
+
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
- 
+
+// Inicia a aplicação pela porta configurada
 app.listen(process.env.PORT || 8080);
